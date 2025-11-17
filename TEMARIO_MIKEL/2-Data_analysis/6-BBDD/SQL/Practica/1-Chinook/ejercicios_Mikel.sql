c74b143cf273fa8e94f5a22bdcc976fea85a6338
Mikel Lizarraga <mikelliza16@hotmail.com>
2025-11-17 23:28:46 +0100

    Update ejercicios_Mikel.sql
    W6 D1

--- TEMARIO_MIKEL/2-Data_analysis/6-BBDD/SQL/Practica/1-Chinook/ejercicios_Mikel.sql
+++ TEMARIO_MIKEL/2-Data_analysis/6-BBDD/SQL/Practica/1-Chinook/ejercicios_Mikel.sql
@@ -1,54 +1,196 @@
+
+PRIMERA PARTE
+
 Ejercicio 01:
 
 SELECT firstname, lastname title FROM customers;
+SELECT *
+FROM Customer
+WHERE Country = 'Brazil';
+
 
 Ejercicio 02:
 
 SELECT lastname FROM employees WHERE title = 'Sales Support Agent';
 
+
+
 Ejercicio 03:
 
-SELECT * FROM tracks WHERE composer LIKE = 'AC/CD'
+SELECT Track.*
+FROM Track
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN Artist ON Album.ArtistId = Artist.ArtistId
+WHERE Artist.Name = 'AC/DC';
+
+
 
 Ejercicio 04:
 
-SELECT firstname || '' || lastname AS Nombre_completo, address || ''|| || 
+SELECT
+    CustomerId AS ID,
+    FirstName || ' ' || LastName AS NombreCompleto,
+    Country
+FROM Customer
+WHERE Country <> 'USA';
+
 
 
 Ejercicio 05:
 
-SELECT firstname || '' || lastname AS Nombre_completo, address || ''|| city || '' ||
+SELECT
+    FirstName || ' ' || LastName AS NombreCompleto,
+    Address,
+    City,
+    State,
+    Country,
+    Email
+FROM Employee
+WHERE Title LIKE '%Sales%';
+
 
 
 Ejercicio 06:
 
 SELECT DISTINCT billingcountry FROM invoices
 
+
+
 Ejercicio 07:
 
 SELECT COUNT(*)state FROM customers WHERE country 'USA' GROUP BY state
 
+
+Ejercicio 08:
+
+SELECT COUNT(*) AS TotalArticulos
+FROM InvoiceLine
+WHERE InvoiceId = 37;
+
+
+Ejercicio 09:
+
+SELECT COUNT(*) AS TotalCanciones
+FROM Track
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN Artist ON Album.ArtistId = Artist.ArtistId
+WHERE Artist.Name = 'AC/DC';
+
+
+
 Ejercicio 10:
 
+SELECT InvoiceId, COUNT(*) AS TotalArticulos
 
 
 Ejercicio 11:
 
+SELECT BillingCountry, COUNT(*) AS TotalFacturas
+
+
+Ejercicio 12:
+
+SELECT BillingCountry, COUNT(*) BETWEEN '2009-01-01' AND '2011-12-31'
+
+
+
+Ejercicio 13:
+
+SELECT COUNT(*) AS TotalFacturas BETWEEN '2009-01-01' AND '2011-12-31';
+
+
+
+Ejercicio 14:
+
+SELECT Country, COUNT(*) AS TotalClientes
+WHERE Country IN ('Spain', 'Brazil')
+
+
+
+Ejercicio 15:
+
+SELECT *
+FROM Track
+WHERE Name LIKE 'You';
+
+
+
+SEGUNDA PARTE
+
+Ejercicio 01:
+
+SELECT 
+    FirstName || ' ' || LastName AS Cliente,
+    InvoiceId,
+    InvoiceDate,
+    BillingCountry
+
+
+Ejercicio 02:
+
+SELECT 
+
+    FirstName || ' ' || LastName AS AgenteVentas,
+    InvoiceId,
+    InvoiceDate
+
+
+Ejercicio 03:
+
+SELECT
+    FirstName || ' ' || c.LastName AS Cliente,
+    Country,
+    FirstName || ' ' || e.LastName AS Agente,
+    Total
+
+
+
+Ejercicio 04:
+
+SELECT 
+    InvoiceId,
+    InvoiceLineId,
+    Name AS Cancion,
+    UnitPrice,
+    Quantity
+
+
 
 Ejercicio 05:
+
+SELECT
+    Name AS Cancion,
+    Name AS Formato,
+    Title AS Album,
+    Name AS Genero
+
+
+
+Ejercicio 06:
+
+SELECT 
+    Name AS Playlist,
+    COUNT(TrackId) AS TotalCanciones
 
 
 
 Ejercicio 07:
+
+SELECT
+    FirstName || ' ' || LastName AS Empleado,
+    SUM(Total) AS TotalVendido
 
 
 
 Ejercicio 08:
 
+SELECT
+    FirstName || ' ' || LastName AS Agente,
+    SUM(Total) AS Total2009
 
 
-Ejercicio 10:
+Ejercicio 09:
 
-
-
-Ejercicio 11:o
+SELECT
+    Name AS Artista,
+    SUM(UnitPrice * Quantity) AS TotalVendido
