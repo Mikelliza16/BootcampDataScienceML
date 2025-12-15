69a8fc8def2c8743ef5b71152ef404091484b9e0
Mikel Lizarraga <mikelliza16@hotmail.com>
2025-12-15 13:29:18 +0100

    W10 D1

--- TEMARIO_MIKEL/3-Machine_Learning/3-Deep_Learning/3-Deep_Learning/1-Intro_Deep_Learning/prueba/funciones.py
+++ TEMARIO_MIKEL/3-Machine_Learning/3-Deep_Learning/3-Deep_Learning/1-Intro_Deep_Learning/prueba/funciones.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 def leer(path):
@@ -9,3 +10,11 @@ def x_y(df, target):
     X = df.drop(columns = [target])
     y = df [target]
     return X, y
+
+def test(X, y, test_size, semilla):
+    X_train, X_test, y_train, y_test =train_test_split(X, y, test_size = test_size, random_state = semilla) 
+    return X_train, X_test, y_train, y_test
+
+def escalar(X_train, X_test):
+    
+    return
